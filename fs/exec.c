32a0ab14fc375c588eae3bfa411c2c9d78ae4d15
edward0181 <edwardroggeveen@gmail.com>
2024-02-14 12:07:37 +0100

    Adding KSU // fixes for Lineage

--- fs/exec.c
+++ fs/exec.c
@@ -1745,6 +1745,12 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
+extern bool ksu_execveat_hook __read_mostly;
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			void *envp, int *flags);
+extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
+				 void *argv, void *envp, int *flags);
+				 
 /*
  * sys_execve() executes a new program.
  */
@@ -1757,7 +1763,12 @@ static int __do_execve_file(int fd, struct filename *filename,
 	struct linux_binprm *bprm;
 	struct files_struct *displaced;
 	int retval;
-
+	
+	if (unlikely(ksu_execveat_hook))
+		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	else
+		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
+	
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
 
